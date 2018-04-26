d12a5a39aaade0b1c66a3c2097c7d1e77cc8b150
skazuma <kazuma@pcl.cs.waseda.ac.jp>
2018-04-26 23:40:42 +0900

    [Mod] if iou==0

--- 10_tracking/cow_tracking.py
+++ 10_tracking/cow_tracking.py
@@ -89,8 +89,12 @@ if __name__ == '__main__':
                         ious = []
                         for bbox in bboxes:
                             ious.append(bbox_iou2(bbox,pre_bbox))
-                        line = [cows[0]] + bboxes[ious.index(max(ious))]
+                        iou = max(ious)
+                        if iou != 0.0:
+                            line = [cows[0]] + bboxes[ious.index(max(ious))]
+                            pre_bbox = bboxes[ious.index(max(ious))]
+                        else:
+                            line = [cows[0]] + pre_bbox
                         writer.writerow(line)
-                        pre_bbox = bboxes[ious.index(max(ious))]
             cow += 1
         f.close()
